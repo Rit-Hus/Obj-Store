5e6a3f81901b64c8236a25b03ab786585ec36266
DottlexD <omaroali918@gmail.com>
2025-04-25 18:41:26 +0200

    Sami's SaleDTO

--- Ineregation/SlaeDTO.java
+++ Ineregation/SlaeDTO.java
@@ -1,5 +1,61 @@
 package Ineregation;
 
 public class SlaeDTO {
-    
+    private double priceTotal;
+private int productAmount;
+private int saleDate;
+private double pricePerItem;
+String  productName;
+
+
+public SlaeDTO( double priceTotal, int productAmount,int saleDate, String productName, double pricePerItem){
+
+ this.priceTotal  = priceTotal;
+this.productAmount = productAmount;
+this.saleDate = saleDate;
+this.productName = productName;
+this.pricePerItem = pricePerItem;
+
+
 }
+/*These are the getters. It creates a well encapsulated code*/
+
+public double getPriceTotal(){
+
+    return priceTotal;
+}
+
+public int getProductAmount(){
+return productAmount;
+}
+
+
+public double getPricePerItem(){
+
+    return pricePerItem;
+}
+
+public int getSaleDate(){
+
+    return saleDate;
+}
+
+public String getProductName(){
+
+return productName;
+
+
+
+}
+/*A method to update the total price*/
+public void updateTotal(double pricePerItem, int vat){
+this.priceTotal =  (pricePerItem * productAmount) * (1 + vat / 100.0);
+
+
+}
+
+
+
+
+}
+
