a665a2126895e832bab3dafab1ca511d8f3a260e
Rit-Hus <ritwan.hussein@hotmail.com>
2025-04-29 10:35:16 +0200

    Payment-update

--- S3-FörstaSeminaruim/Main/Model/Payment.java
+++ S3-FörstaSeminaruim/Main/Model/Payment.java
@@ -2,13 +2,26 @@ package Model;
 
 public class Payment {
     private String paymentMethod;
+    private double amountPaid;
+    private double amountChange;
 
-    public void startPayment(double amount){
-    
+    /**
+     * Performs the payment operation with the specified amount.
+     *
+     * @param amount The amount to be paid.
+     */
+
+    public void pay(double amount){
     }
 
-    public void cancelPayment(){
+    /**
+     * Initializes the payment and calculates the amount of change.
+     *
+     * @param amountPaid The amount paid by the customer.
+     */
 
+    public void payment(double amountPaid){
+     
     }
 
 
