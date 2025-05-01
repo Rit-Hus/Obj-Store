6f2a3082042cf5bd0d92609162c74f85dbd95f5c
Astrososs <sam199823q@gmail.com>
2025-05-01 11:44:13 +0200

    added comment to SaleDTO.java

--- se/kth/iv1350/integration/SaleDTO.java
+++ se/kth/iv1350/integration/SaleDTO.java
@@ -1,11 +1,27 @@
 package se.kth.iv1350.integration;
 
+/**
+ * The SaleDTO class represents a completed sale with details such as the total price,
+ * product amount, sale date, product name, and price per item.
+ * It provides methods to access these details and update the total price.
+ */
 public class SaleDTO{
     private double priceTotal;
 private int productAmount;
 private int saleDate;
 private double pricePerItem;
 String  productName;
+
+
+/**
+ * Creates a new {@code SaleDTO}, which represents a completed sale.
+ *
+ * @param priceTotal The total price of the sale.
+ * @param productAmount The number of products sold.
+ * @param saleDate The date of the sale (e.g., as an integer timestamp or formatted value).
+ * @param productName The name of the sold product.
+ * @param pricePerItem The price per single item.
+ */
 
 
 public SaleDTO( double priceTotal, int productAmount,int saleDate, String productName, double pricePerItem){
@@ -18,28 +34,59 @@ this.pricePerItem = pricePerItem;
 
 
 }
-/*These are the getters. It creates a well encapsulated code*/
 
+/* These are getters which helps to keep SaleDTO well encapsulated */
+
+
+/**        
+ * Gets the total price of the sale.
+ *
+ * @return The total price of the sale.
+ */ 
 public double getPriceTotal(){
 
     return priceTotal;
 }
 
+/**
+ * Gets the amount of products sold.
+ *
+ * @return The number of products sold.
+ */
 public int getProductAmount(){
-return productAmount;
-}
+
+    return productAmount;}
 
 
+
+
+/**
+ * Gets the price per item sold.
+ *
+ * @return The price per item sold.
+ */
 public double getPricePerItem(){
 
     return pricePerItem;
 }
 
+
+/**
+ * Gets the date of the sale.
+ *
+ * @return The date of the sale.
+ */
 public int getSaleDate(){
 
     return saleDate;
 }
 
+
+/**
+ * Gets the name of the product sold.
+ *
+ * @return The name of the product sold.
+ */
 public String getProductName(){
 
 return productName;
