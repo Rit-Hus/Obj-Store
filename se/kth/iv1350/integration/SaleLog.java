4db65aa0fed1b9f637600af87723f73f205ee093
Astrososs <sam199823q@gmail.com>
2025-05-03 16:58:48 +0200

    edited SaleLog

--- se/kth/iv1350/integration/SaleLog.java
+++ se/kth/iv1350/integration/SaleLog.java
@@ -1,8 +1,16 @@
 package se.kth.iv1350.integration;
 
+import java.util.ArrayList;
+
 public class SaleLog {
+
+private ArrayList<SaleDTO> saleDTOList = new ArrayList<>();
     
-public void storesInfo(SaleDTO saleInfo){}
+public void storesInfo(SaleDTO saleInfo){
+
+saleDTOList.add(saleInfo);
+
+}
 
 
 
