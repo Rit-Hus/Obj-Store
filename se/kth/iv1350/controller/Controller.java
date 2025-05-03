56d1ab034f852874f9fd5f80d1438b12cdfc2bc5
DottlexD <omaroali918@gmail.com>
2025-05-03 16:04:02 +0200

    Major Changes

--- se/kth/iv1350/controller/Controller.java
+++ se/kth/iv1350/controller/Controller.java
@@ -1,70 +1,8 @@
 package se.kth.iv1350.controller;
 
-import se.kth.iv1350.integration.ExternalAccountingSystem;
-import se.kth.iv1350.integration.ExternalInventorySystem;
-import se.kth.iv1350.integration.ItemDTO;
-import se.kth.iv1350.integration.SaleDTO;
-import se.kth.iv1350.integration.SaleLog;
-import se.kth.iv1350.model.Receipt;
-import se.kth.iv1350.model.Sale;
 
 public class Controller {
     
-    private ExternalInventorySystem iSystem;
-    private ExternalAccountingSystem aSystem;
-    private SaleLog saleLog;
-    private Sale sale;
-    private Receipt receipt;
-    private ItemDTO cart;
+
   
-
-  //constructer for controller
-  public void controller(SaleLog saleLog, ExternalInventorySystem iSystem, ExternalAccountingSystem aSystem){
-
-        this.saleLog = saleLog;
-        this.iSystem = iSystem;
-        this.aSystem = aSystem;
-  }
-
-  public void startSale(){
-     this.sale = new Sale();
-  } 
-
-  //
-  public void scanitem(ItemDTO cart){
-      sale.scanitems(cart);
-  }
- 
-  public void endScanItem(ItemDTO cart){
-    sale.addItemsTolist();
-}
-
-  /** let's come back for this later and cahnge the void. date(24-04-2025)
-   * create SaleDTO when done 
-   */
-  public void finishSale(){
-     SaleDTO saledto = sale.endsSale();
-     Receipt receipt = new Receipt(saledto); //vi återkommer
-  }
-
-
-  public void entersIdentification(int soicalNumber){
-
-  }
-
-  public void entersAmount(){
-
-  }
-
-  public void startPayment(){
-
-  }
-
-  public void idNewPrice(int soicalNumber){
-
-  }
-
-
-  //SaleDTO????
-
 }
--- se/kth/iv1350/model/Payment.java
+++ se/kth/iv1350/model/Payment.java
@@ -1,52 +1,6 @@
 package se.kth.iv1350.model;
 
 public class Payment {
-    private String paymentMethod;
-    private double amountPaid;
-    private double amountChange;
-
-    /**
-     * Performs the payment operation with the specified amount.
-     *
-     * @param amount The amount to be paid.
-     */
-
-    public void payment(double amount){
-    }
-
-    /**
-     * Initializes the payment and calculates the amount of change.
-     *
-     * @param amountPaid The amount paid by the customer.
-     */
-
-    public void startPayment(double amountPaid){
-     
-    }
-
-
-    //add the comments later
-
-
-    public void cancelPayment(){
-    }
-
-     public double getAmountPaid() {
-        return this.amountPaid;
-    
-     }
-
-
-     public String getPaymentMethod() {
-        return paymentMethod; //let's add if cash was used as a payment method the program will need to round that number but iif card was used that the total won't change
-    }
-     
-
-    public double getAmoutChange(int totalPaid, Sale sale) {
-        //amountChange = totalPaid - (Info from the sale class)
-        return amountChange;
-    }
-
 
 
 }
