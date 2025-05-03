5ee701da0c5010ce4c48372469ca4496af86581f
DottlexD <omaroali918@gmail.com>
2025-05-03 17:44:30 +0200

    Sale Changes

--- se/kth/iv1350/model/Sale.java
+++ se/kth/iv1350/model/Sale.java
@@ -1,76 +1,32 @@
 package se.kth.iv1350.model;
 
 import java.time.LocalDate;
-import se.kth.iv1350.integration.ItemDTO;
-import se.kth.iv1350.integration.SaleDTO;
+import java.util.ArrayList;
 
 public class Sale {
-    private final  java.time.LocalDate saleDate;
-    private se.kth.iv1350.integration.ItemDTO ItemDTO;
-    private ItemDTO[] scannedcart;
+
+    private LocalDate saleDate;
     private double  totalAmount;
-   
-    /**
-    private double  VAT;
-    // private ExternalInventorySystem EI; bra att ha
-   // private ArrayList<Item> items = new ArrayList<>(); behövs inte i våran sitation
-    
-     * Constructor for creating a new Sale object
-     */
-    public Sale() {
-        this.saleDate = LocalDate.now();
+    private int VAT;
+    private ArrayList<Item> items = new ArrayList<>();
 
+
+    public Sale(){
+        saleDate = LocalDate.now();
     }
 
-    /**
-     * Returns the total amount for the sale
-     * @return the total amount for the sale
-     */
 
+    public void updateTotal(){
+        totalAmount = 0;
 
-    public double getToTalAmount() {
-        return this.totalAmount;
-    }
-     
-    
-    
-
-    /**
-     * Adds an items to the itemDTO list
-     * @param ItemDTO the ID of the item to add
-     */
-     // Head of the linked list
-     
-    
-    /**
-     * Checks if an item has been scanned
-     * @param ItemDTO the ID of the item to check
-     * @return true if the item has been scanned, otherwise false
-     */
-
-    public boolean isScanned(ItemDTO item) {
-       int counter = 0;
-       for(ItemDTO scan:this.scannedcart){
-         if(item ==this.scannedcart[counter++]){
-            return true;
-         }
-       }
-       return false;
+        for(Item item : items){
+            totalAmount += item.getQuantity() * item.getPrice() * item.getVat();
+        }
         
+        System.out.println("The Total is now:" + totalAmount);
     }
-    /**
-     * Ends the sale and generates a Receipt object
-     * @param saleDTO the SaleDTO object containing information about the sale
-     * @param payment the Payment object containing information about the payment
-     * @return a Receipt object containing information about the sale and payment
-     */
 
-    public SaleDTO endsSale() {
-    return new SaleDTO(saleDate,scannedcart);
-    }
- 
-  
-   
- //skapa item class
+
+
 
 }
