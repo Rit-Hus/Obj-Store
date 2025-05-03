e52d002873d1bd2f6f57e3a984ca80e66ad5cd1d
DottlexD <omaroali918@gmail.com>
2025-05-03 16:00:36 +0200

    Main Changes

--- se/kth/iv1350/startup/Main.java
+++ se/kth/iv1350/startup/Main.java
@@ -1,11 +1,8 @@
 package se.kth.iv1350.startup;
 
-import se.kth.iv1350.view.View;
-
 public class Main {
     public static void main(String[] args) {
-        View view = new View();
-        view.storeTerminal();
+      
     }
 }
 
