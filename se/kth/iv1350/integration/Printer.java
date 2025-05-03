1b4cbdc4c0ebece18710a53175d763e7fea81e93
DottlexD <omaroali918@gmail.com>
2025-05-03 16:29:02 +0200

    Printer class

--- se/kth/iv1350/integration/Printer.java
+++ se/kth/iv1350/integration/Printer.java
@@ -1,10 +1,18 @@
 package se.kth.iv1350.integration;
 
 
+import se.kth.iv1350.model.Receipt;
+
+
+
 public class Printer {
 
-    public Printer() {
+    public Printer(){
 
+    }
+
+    public void print(Receipt receipt){
+        System.out.println("print kvitto");
     }
 
 }
