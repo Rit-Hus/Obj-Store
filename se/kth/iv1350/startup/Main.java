12e9f4f5aab6ed0234b330e538fb95db53edab49
Rit-Hus <ritwan.hussein@hotmail.com>
2025-05-03 13:53:18 +0200

    Main/View update

--- se/kth/iv1350/startup/Main.java
+++ se/kth/iv1350/startup/Main.java
@@ -1,5 +1,11 @@
 package se.kth.iv1350.startup;
 
+import se.kth.iv1350.view.View;
+
 public class Main {
-    
+    public static void main(String[] args) {
+        View view = new View();
+        view.storeTerminal();
+    }
 }
+
