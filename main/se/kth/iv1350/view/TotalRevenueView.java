2ce81afee24501e056eea652b11aceb4514ee445
Astrososs <sam199823q@gmail.com>
2025-05-31 15:18:14 +0200

    added AbstractRevenueObserver

--- main/se/kth/iv1350/view/TotalRevenueView.java
+++ main/se/kth/iv1350/view/TotalRevenueView.java
@@ -1,24 +1,17 @@
 package main.se.kth.iv1350.view;
 
-import main.se.kth.iv1350.model.RevenueObserver;
-
 /**
- * The TotalRevenueView class implements the RevenueObserver interface and
- * displays the total revenue in the console whenever a new sale is made.
+ * Displays total revenue to the console.
  */
-public class TotalRevenueView implements RevenueObserver {
-    private double totalRevenue = 0.0;
+public class TotalRevenueView extends AbstractRevenueObserver {
 
+    @Override
+    protected void doShowTotalIncome() {
+        System.out.println(">>> Total revenue so far: " + getTotalIncome() + " SEK");
+    }
 
-    /**
-     * This method is called whenever a new sale occurs. It updates the total
-     * revenue and displays the cumulative revenue in the console.
-     *
-     * @param saleAmount The amount of the new sale.
-     */
-    public void onNewSale(double saleAmount) {
-        totalRevenue += saleAmount;
-        String formatted = String.format("%.2f", totalRevenue).replace('.', ',');
-        System.out.println("Total revenue so far: " + formatted + " SEK");
+    @Override
+    protected void handleErrors(Exception e) {
+        System.err.println(">>> Could not display total revenue: " + e.getMessage());
     }
 }
