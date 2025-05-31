835f35ab35deea4ecbabd70be01e789e2f77e902
Astrososs <sam199823q@gmail.com>
2025-05-31 21:51:07 +0200

    inheritance

---
+++ main/se/kth/iv1350/inheritance/InheritedRandom.java
@@ -0,0 +1,15 @@
+package main.se.kth.iv1350.inheritance;
+
+import java.util.Random;
+
+/**
+ * Adapts java.util.Random using inheritance.
+ * Overrides nextInt to always return only even numbers.
+ */
+public class InheritedRandom extends Random {
+    @Override
+    public int nextInt(int bound) {
+        int result = super.nextInt(bound);
+        return result % 2 == 0 ? result : result - 1;
+    }
+}
