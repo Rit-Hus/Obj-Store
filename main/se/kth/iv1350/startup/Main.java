41f8dd459b5b72d0e062aedd39d22428908e16a5
Astrososs <sam199823q@gmail.com>
2025-05-31 16:52:38 +0200

    fixed main duplicate message

--- main/se/kth/iv1350/startup/Main.java
+++ main/se/kth/iv1350/startup/Main.java
@@ -4,7 +4,6 @@ import main.se.kth.iv1350.controller.Controller;
 import main.se.kth.iv1350.integration.ExternalInventorySystem;
 import main.se.kth.iv1350.integration.Printer;
 import main.se.kth.iv1350.integration.TotalRevenueFileOutput;
-import main.se.kth.iv1350.model.RevenueObserver;
 import main.se.kth.iv1350.util.FileLogger;
 import main.se.kth.iv1350.view.TotalRevenueView;
 import main.se.kth.iv1350.view.View;
@@ -13,38 +12,20 @@ import main.se.kth.iv1350.view.View;
 public class Main {
     public static void main(String[] args) {
         Printer printer = new Printer();
-
         ExternalInventorySystem inventorySystem = ExternalInventorySystem.getInstance();
         Controller controller = new Controller(printer, inventorySystem);
 
-        controller.addRevenueObserver((RevenueObserver) new TotalRevenueView());
-        controller.addRevenueObserver(new TotalRevenueFileOutput());
-
-
-
+        // Create and register observers (only once)
         TotalRevenueView consoleObserver = new TotalRevenueView();
         TotalRevenueFileOutput fileObserver = new TotalRevenueFileOutput();
         FileLogger logger = new FileLogger();
 
-        controller.addRevenueObserver((RevenueObserver) consoleObserver);
+        controller.addRevenueObserver(consoleObserver);
         controller.addRevenueObserver(fileObserver);
         controller.addRevenueObserver(logger);
 
-
-
-
+        // Run the view
         View view = new View(controller, printer);
         view.run();
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
     }
 }
