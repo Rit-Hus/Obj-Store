1a05cdd06b869c207cd885a3ebf59865d5ba1677
Omar Ali <99191112+DottlexD@users.noreply.github.com>
2025-06-02 13:19:14 +0200

    Minor changes

--- main/se/kth/iv1350/startup/Main.java
+++ main/se/kth/iv1350/startup/Main.java
@@ -4,7 +4,6 @@ import main.se.kth.iv1350.controller.Controller;
 import main.se.kth.iv1350.integration.ExternalInventorySystem;
 import main.se.kth.iv1350.integration.Printer;
 import main.se.kth.iv1350.integration.TotalRevenueFileOutput;
-import main.se.kth.iv1350.util.FileLogger;
 import main.se.kth.iv1350.view.TotalRevenueView;
 import main.se.kth.iv1350.view.View;
 
@@ -25,7 +24,7 @@ public class Main {
         controller.addRevenueObserver(consoleObserver);
         controller.addRevenueObserver(fileObserver);
 
-        // Run the view
+
         View view = new View(controller, printer);
         view.run();
     }
